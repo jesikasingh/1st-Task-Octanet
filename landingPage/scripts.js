465cdb36a12b7b85bd8a2764f172b9d0c7a93f60
jesikasingh <jesikasingh2808@gmail.com>
2024-07-18 01:30:26 +0530

    Landing-Page

---
+++ landingPage/scripts.js
@@ -0,0 +1,5 @@
+// Add any interactivity here (optional)
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    const nav = document.querySelector('nav ul');
+    nav.classList.toggle('showing');
+});
